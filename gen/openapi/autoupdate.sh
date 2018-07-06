4cf79e986fb5c4347f95eef9df950ce5ee532db3
JetMuffin <564936642@qq.com>
2018-07-06 17:28:12 +0800

    Do not check repo name and repo root
    Signed-off-by: JetMuffin <564936642@qq.com>

--- gen/openapi/autoupdate.sh
+++ gen/openapi/autoupdate.sh
@@ -28,30 +28,11 @@ set -o pipefail
 # - setting file is named "settings" at the root of the repo
 
 SCRIPT_ROOT=$(dirname "${BASH_SOURCE}")
-pushd "${SCRIPT_ROOT}" > /dev/null
-SCRIPT_ROOT=`pwd`
-popd > /dev/null
+DIR_ROOT=$(pwd)
+DIR_NAME=${PWD##*/}
 
-REPO_NAME=$(basename `git rev-parse --show-toplevel`)
-REPO_ROOT=$(git rev-parse --show-toplevel)
+echo "Running command \"${SCRIPT_ROOT}/${DIR_NAME}.sh\" \"${DIR_ROOT}/kubernetes\" \"${DIR_ROOT}/settings\""
 
-if [[ ! -f "${SCRIPT_ROOT}/${REPO_NAME}.sh" ]]; then
-  echo "Repo name \"${REPO_NAME}\" is not a supported language."
-  exit 1
-fi
-
-if [[ ! -d "${REPO_ROOT}/kubernetes" ]]; then
-  echo "Expected folder named \"kubernetes\" at the root of the repo"
-  exit 1
-fi
-
-if [[ ! -f "${REPO_ROOT}/settings" ]]; then
-  echo "Expected setting file to be at the root of the repo"
-  exit 1
-fi
-
-echo "Running command \"${SCRIPT_ROOT}/${REPO_NAME}.sh\" \"${REPO_ROOT}/kubernetes\" \"${REPO_ROOT}/settings\""
-
-"${SCRIPT_ROOT}/${REPO_NAME}.sh" "${REPO_ROOT}/kubernetes" "${REPO_ROOT}/settings"
+"${SCRIPT_ROOT}/${DIR_NAME}.sh" "${DIR_ROOT}/kubernetes" "${DIR_ROOT}/settings"
 
 
